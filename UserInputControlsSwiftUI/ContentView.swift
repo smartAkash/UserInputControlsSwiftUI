cdd7ee53831b721e83d14d8831ef7f0bd2a1221e
Akash <akash.nara@smartsensesolutions.com>
2021-10-15 11:16:54 +0530

    Initial Commit

---
+++ UserInputControlsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UserInputControlsSwiftUI
+//
+//  Created by Akash on 15/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UserInputControlsSwiftUI/UserInputControlsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserInputControlsSwiftUIApp.swift
+//  UserInputControlsSwiftUI
+//
+//  Created by Akash on 15/10/21.
+//
+
+import SwiftUI
+
+@main
+struct UserInputControlsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
